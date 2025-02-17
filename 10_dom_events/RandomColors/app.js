dfd616956557eb0221335c17396472d5d9d4a0b9
James Rooke <me@jamesrooke.blog>
2025-02-17 10:35:08 -0500

    form events project

--- 10_dom_events/RandomColors/app.js
+++ 10_dom_events/RandomColors/app.js
@@ -1,16 +1,19 @@
+// Elements
 const button = document.querySelector('button');
 const h1 = document.querySelector('h1');
 
-button.addEventListener('click', function () {
-    const newColor = makeRandColor();
-    document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
-})
-
-const makeRandColor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
+// Functions
+const genRandomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`;
 }
+
+// Event Listeners
+button.addEventListener('click', () => {
+    const newColor = genRandomColor();
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+});
 
--- 10_dom_events/TheEventObject/app.js
+++ 10_dom_events/TheEventObject/app.js
@@ -1,31 +1,27 @@
-document.querySelector('button').addEventListener('click', function (evt) {
-    console.log(evt)
-})
+// document.querySelector('button').addEventListener('click', function(evt) {
+//     console.log(evt)
+// });
 
 // const input = document.querySelector('input');
-// input.addEventListener('keydown', function (e) {
-//     console.log(e.key)
-//     console.log(e.code)
-// })
-// input.addEventListener('keyup', function () {
-//     console.log("KEYUP")
-// })
+// input.addEventListener('keydown', function(evt) {
+//     console.log(`${evt.key} was pressed`);
+// });
 
-window.addEventListener('keydown', function (e) {
-    switch (e.code) {
+window.addEventListener('keydown', function(evt) {
+    switch(evt.code) {
         case 'ArrowUp':
-            console.log("UP!");
+            console.log('UP!');
             break;
         case 'ArrowDown':
-            console.log("DOWN!");
+            console.log('DOWN!');
             break;
         case 'ArrowLeft':
-            console.log("LEFT!");
+            console.log('LEFT!');
             break;
         case 'ArrowRight':
-            console.log("RIGHT!");
-            break
+            console.log('RIGHT!');
+            break;
         default:
-            console.log("IGNORED!")
-    }
-})
+            console.log('IGNORED!');
+    } 
+});
