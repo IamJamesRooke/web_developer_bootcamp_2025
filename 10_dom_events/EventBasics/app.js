b43bf8645b94c5fdd8591b0b4593073b6489e77b
James Rooke <me@jamesrooke.blog>
2025-02-17 09:44:15 -0500

    tutorial on even listeners

--- 10_dom_events/EventBasics/app.js
+++ 10_dom_events/EventBasics/app.js
@@ -1,39 +1,45 @@
 const btn = document.querySelector('#v2');
 
-btn.onclick = function () {
-    console.log("YOU CLICKED ME!")
-    console.log("I HOPE IT WORKED!!")
+btn.onclick = function() {
+    console.log('You clicked me!');
+    console.log('I hope it worked!');
 }
 
 function scream() {
-    console.log("AAAAAHHHHH");
-    console.log("STOP TOUCHING ME!")
+    console.log('AAAAHHHH');
+    console.log('STOP TOUCHING ME!');
 }
 
 btn.onmouseenter = scream;
 
-
-document.querySelector('h1').onclick = () => {
-    alert('you clicked the h1!')
+document.querySelector('h1').onmouseenter = function() {
+    console.log('Passing over h1');
 }
 
+document.querySelector('h1').onmouseleave = function() {
+    console.log('Leaving h1');
+}
+
+document.querySelector('h1').onmousemove = function() {
+    console.log('Movement within h1');
+}
 
 const btn3 = document.querySelector('#v3');
-btn3.addEventListener('click', function () {
-    alert("CLICKED!");
-})
+btn3.addEventListener('mouseup', function() {
+    alert('Clicked!');
+});
 
 function twist() {
-    console.log("TWIST!")
+    console.log('Twist');
 }
+
 function shout() {
-    console.log("SHOUT!")
+    console.log('Shout');
 }
 
 const tasButton = document.querySelector('#tas');
 
-// tasButton.onclick = twist;
-// tasButton.onclick = shout;
-
-tasButton.addEventListener('click', twist)
-tasButton.addEventListener('click', shout)
+tasButton.onclick = () => {
+    twist();
+    shout();
+}
