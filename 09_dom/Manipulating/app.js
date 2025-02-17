dcdf1d5ed329669fe4e363e19d605da788760fe5
James Rooke <me@jamesrooke.blog>
2025-02-17 09:06:42 -0500

    starting DOM events lessons

--- 09_dom/Manipulating/app.js
+++ 09_dom/Manipulating/app.js
@@ -1,12 +1,38 @@
-const allLinks = document.querySelectorAll('a');
+// const allLinks = document.querySelectorAll('a');
+
+// // for (let link of allLinks) {
+// //     link.innerText = 'I AM A LINK!!!!'
+// // }
+
+
+// for (let link of allLinks) {
+//     link.style.color = 'rgb(0, 108, 134)';
+//     link.style.textDecorationColor = 'magenta';
+//     link.style.textDecorationStyle = 'wavy'
+// }
+
+// const allLinks = document.querySelectorAll('a');
 
 // for (let link of allLinks) {
 //     link.innerText = 'I AM A LINK!!!!'
 // }
 
+// const h1 = document.querySelector('h1');
 
-for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
-    link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
-}
+// for (let char of h1.innerText) {
+//     console.log(char);
+// }
+
+// const firstLink = document.querySelector('a');
+// console.log(firstLink.href);
+
+// const allLinks = document.querySelectorAll('a');
+// for (let link of allLinks) {
+//     link.style.color = 'rgb(0, 108, 134)';
+//     link.style.textDecorationColor = 'magenta';
+//     link.style.textDecorationStyle = 'wavy'
+// }
+
+// const h1 = document.querySelector('h1');
+
+// console.log(`H1 Font Family: ${window.getComputedStyle(h1).fontFamily}`);
