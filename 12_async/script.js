6249ef5f2ead05fa5a249f6a90b92a79b7791750
James Rooke <me@jamesrooke.blog>
2025-02-18 10:18:33 -0500

    starting 289: AJAX

--- 12_async/script.js
+++ 12_async/script.js
@@ -69,20 +69,56 @@
 //   })
 //   .catch((err) => {console.log('Request Failed', err)})
 
-const delayedColorChange = (color, delay) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      document.body.style.backgroundColor = color;
-      resolve();
-    }, delay)
-  })
-}
+// const delayedColorChange = (color, delay) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = color;
+//       resolve();
+//     }, delay)
+//   })
+// }
 
-delayedColorChange('red', 1000)
-  .then(() => delayedColorChange('orange', 1000))
-  .then(() => delayedColorChange('yellow', 1000))
-  .then(() => delayedColorChange('green', 1000))
-  .then(() => delayedColorChange('blue', 1000))
-  .then(() => delayedColorChange('indigo', 1000))
-  .then(() => delayedColorChange('violet', 1000))
-  .then(() => delayedColorChange('white', 1000))
+// delayedColorChange('red', 1000)
+//   .then(() => delayedColorChange('orange', 1000))
+//   .then(() => delayedColorChange('yellow', 1000))
+//   .then(() => delayedColorChange('green', 1000))
+//   .then(() => delayedColorChange('blue', 1000))
+//   .then(() => delayedColorChange('indigo', 1000))
+//   .then(() => delayedColorChange('violet', 1000))
+//   .then(() => delayedColorChange('white', 1000))
+
+// const function_works = true;
+
+// const sing = async () => {
+//   if (function_works) {
+//     return 'LA LA LA LA'
+//   } else {
+//     throw 'OH NO, PROBLEM!'
+//   }
+// }
+
+// sing()
+// .then((data) => {
+//   console.log('Promise Resolved with:', data)
+// })
+// .catch((err) => {
+//   console.log('OH NO, PROMISE REJECTED!')
+//   console.log(err)
+// })
+
+// Using async
+// const login = async (username, password) => {
+//   if (!username || !password) throw 'Missing Credentials'
+//   if (password === 'mypassword') return 'Logged In'
+//   throw 'Invalid Password'
+// }
+
+// login('asdfsdfsad', 'mypassword')
+//   .then(msg => {
+//     console.log('Here is the message:');
+//     console.log(msg);
+//   })
+//   .catch (err => {
+//   console.log('OH NO, ERROR!')
+//   console.log(err)
+//   })
