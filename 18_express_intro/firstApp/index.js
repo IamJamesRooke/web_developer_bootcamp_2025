7c283160daf1b79fbec3e4ab5223c5e3635c3fbb
IamJamesRooke <me@jamesrooke.blog>
2025-02-27 13:00:14 -0500

    learning routing

--- 18_express_intro/firstApp/index.js
+++ 18_express_intro/firstApp/index.js
@@ -1,3 +1,18 @@
 const express = require('express');
 const app = express();
-console
+console.dir(app);
+
+// This runs every time the webpage is called.
+app.use((req, res) => {
+    console.log('NEW REQUEST');
+    res.send('<h1>---NEW RESPONSE---</h1>');
+})
+
+app.listen(port=8080, () => {
+    console.log(`LISTENING ON PORT ${port}`);
+})
+
+// app.get('*', (req, res) => {
+//     Change that line to exactly this:
+    
+// app.get(/(.*)/, (req, res) => {
