bcb7f1361621a232852e5ac67ac5727bf92f53b2
IamJamesRooke <me@jamesrooke.blog>
2025-02-28 15:21:41 -0500

    basics of post and get

--- 20_RESTful_routes/index.js
+++ 20_RESTful_routes/index.js
@@ -5,6 +5,10 @@ app.get('/tacos', (req, res) => {
     res.send('GET /tacos response');
 });
 
+app.post('/tacos', (req, res) => {
+    res.send('POST /tacos response');
+});
+
 app.listen(3000, () => { 
-    console.log('App listening on port 3000!'); 
+    console.log('Listening on port 3000!'); 
 });
