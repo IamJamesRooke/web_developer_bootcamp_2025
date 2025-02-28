7510201d78e4b004d0350b7fe0eb68ec89507704
IamJamesRooke <me@jamesrooke.blog>
2025-02-28 09:29:37 -0500

    looping with ejs

--- 19_templating/index.js
+++ 19_templating/index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
 
-app.set('viewengine', 'ejs');
-app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs'); // Fixed typo: 'viewengine' to 'view engine'
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-    res.render('home.ejs')
-})
+    res.render('home.ejs');
+});
 
-app.listen(port=8080, () =>{
-    console.log(`Listening on port ${port}.`)
-})
+app.get('/random', (req, res) => {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    res.render('random.ejs', { randomNumber: randomNumber });
+});
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit.ejs', { subreddit });
+});
+
+app.listen(8080, () => {
+    console.log(`Listening on port 8080.`);
+});
