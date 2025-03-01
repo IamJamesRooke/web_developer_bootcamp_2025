89934968feed3df07749c39f176a4747921cb2e9
IamJamesRooke <me@jamesrooke.blog>
2025-03-01 13:11:05 -0500

    delete functionality added

--- 20_RESTful_routes/index.js
+++ 20_RESTful_routes/index.js
@@ -11,7 +11,7 @@ app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: 'Todd',
@@ -70,6 +70,13 @@ app.patch('/comments/:id', (req, res) => {
     foundComment.comment = newCommentText;
     res.redirect('/comments')
 })
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
+})
+
 
 app.get('/tacos', (req, res) => {
     res.send('GET /tacos response');
