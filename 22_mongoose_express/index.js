8862f599fbf588800e83f10b9fc9696e8a5abcc5
IamJamesRooke <me@jamesrooke.blog>
2025-03-03 18:44:06 -0500

    completed CRUD with mongoose and express

--- 22_mongoose_express/index.js
+++ 22_mongoose_express/index.js
@@ -25,11 +25,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.get('/products', async (req, res) => {
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category });
+        res.render('products/index', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category: 'All' });
+    }
+
     const products = await Product.find({})
     res.render('products/index', { products });
 });
 
 app.get('/products/new', (req, res) => {
+
     res.render('products/new', { categories });
 });
 
