4f55e104928237afc34406d20d8b6faa76ae2645
IamJamesRooke <me@jamesrooke.blog>
2025-03-03 13:29:56 -0500

    mongoose schemas

--- 21_mongoose_basics/index.js
+++ 21_mongoose_basics/index.js
@@ -1,10 +1,27 @@
+// Code to connect to MongoDB using Mongoose
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/movieApp')
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-        console.log('Error connecting to MongoDB', err);
+
+// Define a schema
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+// Create a model
+const Movie = mongoose.model('Movie', movieSchema);
+// const amadeus = new Movie({ title: 'Amadeus', year: 1984, score: 9.2, rating: 'R' });
+
+Movie.insertMany([
+    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+])
+    .then(data => {
+        console.log('It worked!');
+        console.log(data);
     });
-
-
