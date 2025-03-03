f9bdb9150acefc9ea19f2cb4805f6a9e04301dce
IamJamesRooke <me@jamesrooke.blog>
2025-03-03 14:05:46 -0500

    combining mongoose and express

---
+++ 22_mongoose_express/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand')
+    .then(() => {
+        console.log('Mongo connection open');
+    })
+    .catch(err => {
+        console.log('Mongo connection error');
+        console.log(err);
+    });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
