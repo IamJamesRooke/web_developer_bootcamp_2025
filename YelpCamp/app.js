086279faa5092331bfe6a4efcbc4190e74149c7c
IamJamesRooke <me@jamesrooke.blog>
2025-03-04 09:30:01 -0500

    create new campgrounds in db

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -24,6 +24,8 @@ db.once('open', () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }))
+
 // Home Route
 app.get('/', (req, res) => {	
     res.render('home');
@@ -34,10 +36,21 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground })
 })
+
 
 // Listen on Port 3000
 app.listen(3000, () => {
