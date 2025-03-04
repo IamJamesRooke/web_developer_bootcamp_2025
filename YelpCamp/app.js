fa295daaebe8e20314ffa110b1399ca888ce208b
IamJamesRooke <me@jamesrooke.blog>
2025-03-04 10:48:05 -0500

    campground delete

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -65,6 +65,11 @@ app.put('/campgrounds/:id', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 })
 
+app.delete('/campgrounds/:id', async(req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
+});
 
 // Listen on Port 3000
 app.listen(3000, () => {
