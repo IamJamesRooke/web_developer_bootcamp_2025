d4f8a99d19f3c1710ba485b2742d140512acf45d
IamJamesRooke <me@jamesrooke.blog>
2025-03-05 10:43:31 -0500

    refactored with  boilerplate

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate')
 
 const methodOverride = require('method-override')
 
@@ -23,6 +24,7 @@ db.once('open', () => {
 });
 
 // EJS Imports
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
