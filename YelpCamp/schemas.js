9a77372e04fcab3aa044a454bcda917072fd130c
IamJamesRooke <me@jamesrooke.blog>
2025-03-07 12:02:14 -0500

    validating reviews with joi

--- YelpCamp/schemas.js
+++ YelpCamp/schemas.js
@@ -1,3 +1,9 @@
+/**
+ * This module defines Joi validation schemas for the YelpCamp application.
+ * It includes schemas for validating campground and review data.
+ * Joi is used to ensure that the data meets the required structure and constraints.
+ */
+
 const Joi = require('joi');
 
 module.exports.campgroundSchema = Joi.object({
@@ -9,3 +15,10 @@ module.exports.campgroundSchema = Joi.object({
         description: Joi.string().required()
     }).required()
 });
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required(),
+    })
+})
