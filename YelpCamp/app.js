0effbc368805dddd692887de8a025f58b7b9d48b
IamJamesRooke <me@jamesrooke.blog>
2025-03-06 19:24:30 -0500

    built and styled error page

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require('path');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override');
 
 // Mongoose Imports
@@ -11,10 +12,7 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 
 // Mongoose Connection
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
 // Mongoose Connection Events
 const db = mongoose.connection;
@@ -46,6 +44,7 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -75,8 +74,14 @@ app.delete('/campgrounds/:id', catchAsync(async(req, res) => {
     res.redirect('/campgrounds')
 }));
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
 app.use((err, req, res, next) => {
-    res.send('ERROR FOUND')
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = 'Something went wrong.'
+    res.status(statusCode).render('error', { err });
 })
 
 // Listen on Port 3000
