bd94f48d2308a094f6ad523067b623d1323ad7f4
IamJamesRooke <me@jamesrooke.blog>
2025-03-28 12:03:53 -0500

    starting express sessions, initial setup

--- 24_express_router/index.js
+++ 24_express_router/index.js
@@ -9,9 +9,9 @@ app.use(`/shelters`, shelterRoutes);
 app.use(`/dogs`, dogRoutes);
 app.use(`/admin`, adminRoutes);
 
-// Cookies
+// COOKIES START
 const cookieParser = require('cookie-parser');
-app.use(cookieParser())
+app.use(cookieParser('secret-key'))
 
 app.get('/greet', (req, res) => {
    const { name = 'No Name'} = req.cookies;
@@ -23,6 +23,17 @@ app.get('/setname', (req, res) => {
     res.cookie('color', 'Blue');
     res.send('Cookie Sent');
 })
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', { signed: true})
+    res.send('Cookie Signed')
+})
+
+app.get('/verifyfruit', (req, res) => {
+    console.log(req.cookies);
+    res.send(req.signedCookies);
+})
+// COOKIES END
 
 app.listen(port=3000, () => {
     console.log(`Serving app on localhost:${port}`);
