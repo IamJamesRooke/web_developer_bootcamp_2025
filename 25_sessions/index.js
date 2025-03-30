6b47b2fbee40c5ee3d9ec5619db70c52c7d27c3d
IamJamesRooke <me@jamesrooke.blog>
2025-03-30 08:43:43 -0500

    view counter

--- 25_sessions/index.js
+++ 25_sessions/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 
+const session = require('express-session');
+
+app.use(session({secret: 'sessionsecret'}));
+
+app.get('/viewcount', (req, res) => {
+    if (req.session.count) {
+        req.session.count += 1;
+    } else {
+        req.session.count = 1;
+    }
+    
+    res.send(`Page Views: ${req.session.count}`)
+})
+
+
+
 app.listen(3000, () => {
     console.log('Listening on PORT 3000')
 })
