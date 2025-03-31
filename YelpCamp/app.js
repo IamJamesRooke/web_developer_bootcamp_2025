9ac914fc007ad0079fbdfc22bb276380dd758998
IamJamesRooke <me@jamesrooke.blog>
2025-03-31 09:10:43 -0500

    sessiones craeted

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session')
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 
@@ -22,12 +23,25 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride('_method'));
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
 
 app.get('/', (req, res) => {
     res.render('home')
