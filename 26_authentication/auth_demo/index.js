6f6e3a73d1c218e28f9e07949a6c6f81249fff93
IamJamesRooke <me@jamesrooke.blog>
2025-03-31 14:28:52 -0500

    login functionality done, starting log out functionality

--- 26_authentication/auth_demo/index.js
+++ 26_authentication/auth_demo/index.js
@@ -3,8 +3,9 @@ const app = express();
 const User = require('./models/user')
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const session = require('express-session')
 
-mongoose.connect('mongodb://localhost:27017/authDemo')
+mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
     console.log('MONGO CONNECTION OPEN')
 }).catch(err => {
@@ -15,6 +16,7 @@ app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(express.urlencoded({extended: true}));
+app.use(session({ secret: 'thisisabettersecret!', resave: false, saveUninitialized: true }));
 
 
 app.get('/', (req, res) => {
@@ -33,6 +35,7 @@ app.post('/register', async (req, res) => {
         password: hash
     })
     await user.save();
+    req.session.user_id = user._id;
     res.redirect('/')
 
 })
@@ -43,18 +46,22 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username })
-    const validPassword = await bcrypt.compare(password, user.password)
-    if(validPassword) {
-        res.send('Yay')
-    } else {
-        res.send('Try Again')
+    const user = await User.findOne({ username });
+    if (user) {
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (validPassword) {
+            req.session.user_id = user._id;
+            return res.redirect('/secret');
+        }
     }
-    
+    res.redirect('/login');
 })
 
 app.get('/secret', (req, res) => {
-    res.send ('Cannot see unless logged in.')
+    if (!req.session.user_id) {
+        return res.redirect('/login');
+    }
+    res.send('secret'); // Ensure you have a 'secret.ejs' file in your views folder
 })
 
 
