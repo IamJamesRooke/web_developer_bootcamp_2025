359645dde0763d9133875c18533a5d4bf4d832ea
IamJamesRooke <me@jamesrooke.blog>
2025-04-01 12:13:40 -0500

    section 51 authentication complete

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -62,6 +62,7 @@ app.get('/fakeUser', async (req, res) => {
 })
 
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
