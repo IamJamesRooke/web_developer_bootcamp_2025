b4ed36ff4581a4e1d229555d7c97b9755553c8c3
IamJamesRooke <me@jamesrooke.blog>
2025-04-08 10:13:37 -0500

    included mongo sanitize to prevent SQL injection

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -14,6 +14,7 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const  mongoSanitize = require('express-mongo-sanitize');
 
 
 const campgroundRoutes = require('./routes/campgrounds');
@@ -36,9 +37,10 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 // Ensure the 'public' directory contains your CSS/JS files for the navbar.
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize);
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
